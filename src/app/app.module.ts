aadbae354e36a8a7e41954ffc371b45e676a08b9
shinyoshiaki <shinyoshiaki2012@gmail.com>
2019-02-26 11:05:01 +0900

    add service worker

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,10 +6,12 @@ import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
 import { SelectFileComponent } from "./components/select-file/select-file.component";
 import { DownloadComponent } from "./components/download/download.component";
+import { ServiceWorkerModule } from '@angular/service-worker';
+import { environment } from '../environments/environment';
 
 @NgModule({
   declarations: [AppComponent, SelectFileComponent, DownloadComponent],
-  imports: [BrowserModule, AppRoutingModule, FormsModule],
+  imports: [BrowserModule, AppRoutingModule, FormsModule, ServiceWorkerModule.register('ngsw-worker.js', { enabled: environment.production })],
   providers: [],
   bootstrap: [AppComponent]
 })
